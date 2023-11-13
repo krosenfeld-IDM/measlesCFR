2977059d8b7af0ab1373dfc01d7960969ce506d5
Katherine Rosenfeld <krosenf@gmail.com>
2023-11-13 11:30:59 -0800

    monthly resolution through 5 years and then yearly after

--- dose_scheduling/get_CFR_estimates.R
+++ dose_scheduling/get_CFR_estimates.R
@@ -6,10 +6,10 @@ library(dplyr)
 setwd("/mnt")
 
 country.list <- listOfCountries()
-short.country.list <- country.list[1:3]
+short.country.list <- c('NGA')
 
-predictAgeCFR <- function(country, age) {
-  df <- predictCFR(country=country, start_age=age, end_age=1, start_year=2010, end_year=2019)
+predictAgeCFR <- function(country, start_age, end_age) {
+  df <- predictCFR(country=country, start_age=start_age, end_age=end_age, start_year=2019, end_year=2019)
   return(df)
   }
 
@@ -18,13 +18,23 @@ concatenated_function <- function(strings) {
   # Initialize an empty data frame to store the results
   result_df <- data.frame()
   
+  # first do [0, 5) years at monthly resolution
+  ages <- seq(0, 5*12-1) / 12
   for (country in strings) {
-    for (age in c(3/12, 6/12, 9/12, 12/12)) {
+    for (age in ages) {
       # Call predictCFR on each string
-      df <- predictAgeCFR(country, age)
+      df <- predictAgeCFR(country, age, ceiling(age))
       # Concatenate the resulting data frame to result_df
       result_df <- bind_rows(result_df, df)      
     }
+  }
+  
+  # then do yearly bins from 5 years out to 40
+  for (country in strings) {
+      # Call predictCFR 
+      df <- predictAgeCFR(country, 5, 40)
+      # Concatenate the resulting data frame to result_df
+      result_df <- bind_rows(result_df, df)      
   }
   
   return(result_df)
